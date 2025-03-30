a6c5ac0124b73d1697682a5cfddf2ae68332cfea
canisminor1990 <i@canisminor.cc>
2025-03-30 19:23:09 +0800

    ♻️ refactor: Refactor agent settings

--- src/app/[variants]/(main)/chat/(workspace)/features/AgentSettings/index.tsx
+++ src/app/[variants]/(main)/chat/(workspace)/features/AgentSettings/index.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { Drawer } from 'antd';
-import { useResponsive, useTheme } from 'antd-style';
+import { Drawer } from '@lobehub/ui';
+import { Skeleton } from 'antd';
+import { useResponsive } from 'antd-style';
 import isEqual from 'fast-deep-equal';
-import { memo, useRef, useState } from 'react';
+import { Suspense, memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
-import Header from '@/app/[variants]/(main)/settings/_layout/Desktop/Header';
+import BrandWatermark from '@/components/BrandWatermark';
+import PanelTitle from '@/components/PanelTitle';
 import { INBOX_SESSION_ID } from '@/const/session';
 import AgentChat from '@/features/AgentSetting/AgentChat';
 import AgentMeta from '@/features/AgentSetting/AgentMeta';
@@ -21,6 +23,7 @@ import { useInitAgentConfig } from '@/hooks/useInitAgentConfig';
 import { useAgentStore } from '@/store/agent';
 import { agentSelectors } from '@/store/agent/slices/chat';
 import { ChatSettingsTabs } from '@/store/global/initialState';
+import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
 import { useSessionStore } from '@/store/session';
 import { sessionMetaSelectors } from '@/store/session/selectors';
 
@@ -31,7 +34,7 @@ const AgentSettings = memo(() => {
   const id = useSessionStore((s) => s.activeId);
   const config = useAgentStore(agentSelectors.currentAgentConfig, isEqual);
   const meta = useSessionStore(sessionMetaSelectors.currentAgentMeta, isEqual);
-
+  const { enablePlugins } = useServerConfigStore(featureFlagsSelectors);
   const { isLoading } = useInitAgentConfig();
   const [showAgentSetting, updateAgentConfig] = useAgentStore((s) => [
     s.showAgentSetting,
@@ -45,9 +48,7 @@ const AgentSettings = memo(() => {
 
   const [tab, setTab] = useState(isInbox ? ChatSettingsTabs.Prompt : ChatSettingsTabs.Meta);
 
-  const ref = useRef<any>(null);
-  const theme = useTheme();
-  const { md = true, mobile = false } = useResponsive();
+  const { md = true } = useResponsive();
 
   const category = <CategoryContent setTab={setTab} tab={tab} />;
   return (
@@ -60,56 +61,46 @@ const AgentSettings = memo(() => {
       onMetaChange={updateAgentMeta}
     >
       <Drawer
+        containerMaxWidth={1280}
         height={'100vh'}
-        onClose={() => {
-          useAgentStore.setState({ showAgentSetting: false });
-        }}
+        noHeader
+        onClose={() => useAgentStore.setState({ showAgentSetting: false })}
         open={showAgentSetting}
         placement={'bottom'}
+        sidebar={
+          md && (
+            <Flexbox
+              gap={20}
+              style={{
+                minHeight: '100%',
+              }}
+            >
+              <PanelTitle desc={t('header.sessionDesc')} title={t('header.session')} />
+              {category}
+              <BrandWatermark paddingInline={12} />
+            </Flexbox>
+          )
+        }
+        sidebarWidth={280}
         styles={{
-          body: { padding: 0 },
-          content: {
-            background: theme.colorBgContainer,
+          sidebarContent: {
+            gap: 48,
+            justifyContent: 'space-between',
+            minHeight: '100%',
+            paddingBlock: 24,
+            paddingInline: 48,
           },
         }}
-        title={t('header.session')}
       >
-        <Flexbox height={'100%'} horizontal={md} ref={ref} width={'100%'}>
-          {md ? (
-            <Flexbox padding={16}>{category}</Flexbox>
-          ) : (
-            <Header
-              getContainer={() => ref.current}
-              title={t(`agentTab.${tab as ChatSettingsTabs}`)}
-            >
-              {category}
-            </Header>
-          )}
-          <Flexbox
-            align={'center'}
-            gap={mobile ? 0 : 64}
-            paddingInline={mobile ? 0 : 56}
-            style={{
-              background: mobile
-                ? theme.colorBgContainer
-                : theme.isDarkMode
-                  ? theme.colorFillQuaternary
-                  : theme.colorBgElevated,
-              minHeight: '100%',
-              overflowX: 'hidden',
-              overflowY: 'auto',
-              paddingTop: mobile ? 0 : 16,
-            }}
-            width={'100%'}
-          >
-            {tab === ChatSettingsTabs.Meta && <AgentMeta />}
-            {tab === ChatSettingsTabs.Prompt && <AgentPrompt />}
-            {tab === ChatSettingsTabs.Chat && <AgentChat />}
-            {tab === ChatSettingsTabs.Modal && <AgentModal />}
-            {tab === ChatSettingsTabs.TTS && <AgentTTS />}
-            {tab === ChatSettingsTabs.Plugin && <AgentPlugin />} <Footer />
-          </Flexbox>
-        </Flexbox>
+        <Suspense fallback={<Skeleton paragraph={{ rows: 6 }} title={false} />}>
+          {tab === ChatSettingsTabs.Meta && <AgentMeta />}
+          {tab === ChatSettingsTabs.Prompt && <AgentPrompt />}
+          {tab === ChatSettingsTabs.Chat && <AgentChat />}
+          {tab === ChatSettingsTabs.Modal && <AgentModal />}
+          {tab === ChatSettingsTabs.TTS && <AgentTTS />}
+          {enablePlugins && tab === ChatSettingsTabs.Plugin && <AgentPlugin />}
+        </Suspense>
+        <Footer />
       </Drawer>
     </AgentSettingsProvider>
   );
